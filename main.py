84e93ef19b525cf3f1eea63e4d1432bfaddee9f1
Gabriel N. V. Resende <gabrielnvresende@gmail.com>
2024-02-09 18:16:10 -0300

    Solved the IDE problem partially and included the chrome driver file in the folder
    solved the specific IDE problem, allowing the program to scrape the data. However, it is still unable to open the tab

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+
+
+path_to_chromedriver = "C:\\Users\\gabri\\OneDrive\\Área de Trabalho\\GabTheFolder\\Python projects\\PYTHON-AUTOMATION-PROJECTS\\chromedriver-win64\\chromedriver.exe"
+
+service = Service(executable_path=path_to_chromedriver)
+
+
 
 def get_driver():
     optionsForSearch = webdriver.ChromeOptions()
@@ -7,25 +16,31 @@ def get_driver():
 
     #Remvoes parts of the webpage that may interfere with the scraping
     optionsForSearch.add_argument("disable-infobars")
+    
     #Starts the webpage as maximized --> the page may change shape to adapt to the device
     optionsForSearch.add_argument("start-maximized")
-    #Avoid issues that occur when interacting with linux machines
-    optionsForSearch.add_argument("disable-dev-shm-usage")
+     
     #Remvoe sandboxes (used for security)
     optionsForSearch.add_argument("no-sandbox")
-    ##
+    
+    
     optionsForSearch.add_experimental_option("excludeSwitches", ["enable-automation"])
     optionsForSearch.add_argument("disable-blank-features=AutomationControlled")
+    
 
-    driver = webdriver.Chrome(options=optionsForSearch)
-    #Connects driver to webpage, just enter the URL
-    driver.get("https://automated.pythonanywhere.com/")
+    
+    driver = webdriver.Chrome(service=service, options=optionsForSearch)       #add the service attribute to fix the IDE problem
+
+    URL = "https://automated.pythonanywhere.com/"   #Connects driver to webpage, just enter the URL
+    driver.get(URL)
 
     return driver
 
+#For the xpath, ommit the /text() at the end
 def main():
     driver = get_driver()
-    element = driver.find_element(by= "xpath", value="/html/body/div[1]/div/h1[1]/text()")
+    element = driver.find_element(by="xpath", 
+                                  value="/html/body/div[1]/div/h1[1]")
     return element.text
 
-print(main)
+print(main())
