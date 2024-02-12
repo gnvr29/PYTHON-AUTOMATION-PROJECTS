ed7fcd5ff099f69b593a81eb7e5ff2884128f083
Gabriel N. V. Resende <gabrielnvresende@gmail.com>
2024-02-12 19:02:47 -0300

    Copied whole scrapingSimpleDataWithSelenium.py

---
+++ automatingLogin.py
@@ -0,0 +1,49 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+
+
+path_to_chromedriver = "C:\\Users\\gabri\\OneDrive\\Área de Trabalho\\GabTheFolder\\Python projects\\PYTHON-AUTOMATION-PROJECTS\\chromedriver-win64\\chromedriver.exe"
+
+service = Service(executable_path=path_to_chromedriver)
+
+
+
+def get_driver():
+    optionsForSearch = webdriver.ChromeOptions()
+
+    #Set options to make browsing easier
+
+    #Remvoes parts of the webpage that may interfere with the scraping
+    optionsForSearch.add_argument("disable-infobars")
+    
+    #Starts the webpage as maximized --> the page may change shape to adapt to the device
+    optionsForSearch.add_argument("start-maximized")
+     
+    #Remvoe sandboxes (used for security)
+    optionsForSearch.add_argument("no-sandbox")
+    
+    
+    optionsForSearch.add_experimental_option("excludeSwitches", ["enable-automation"])
+    optionsForSearch.add_argument("disable-blank-features=AutomationControlled")
+
+    #fixes the problem of the tab immediately closing after the code execution
+    optionsForSearch.add_experimental_option("detach", True)
+    
+
+    
+    driver = webdriver.Chrome(service=service, options=optionsForSearch)       #add the service attribute to fix the IDE problem
+
+    URL = "https://automated.pythonanywhere.com/login/"   #Connects driver to webpage, just enter the URL
+    driver.get(URL)
+
+    return driver
+
+#For the xpath, ommit the /text() at the end, otherwsie, it won't work
+def main():
+    driver = get_driver()
+    element = driver.find_element(by="xpath", 
+                                  value="/html/body/div[1]/div/h1[1]")
+    return element.text
+
+print(main())
