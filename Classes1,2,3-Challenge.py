e802e09d935e157d1baf96613818d365e27366eb
Gabriel N. V. Resende <gabrielnvresende@gmail.com>
2024-02-12 19:52:14 -0300

    Solved the problem given in the course
    Logged in the page, clicked on the home icon, then scraped the dynamic data in there

--- Classes1,2,3-Challenge.py
+++ Classes1,2,3-Challenge.py
@@ -1,8 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
 import time
-
-#data is being extracted from https://automated.pythonanywhere.com/
 
 path_to_chromedriver = "C:\\Users\\gabri\\OneDrive\\Área de Trabalho\\GabTheFolder\\Python projects\\PYTHON-AUTOMATION-PROJECTS\\chromedriver-win64\\chromedriver.exe"
 
@@ -10,7 +9,7 @@ service = Service(executable_path=path_to_chromedriver)
 
 
 
-def get_driver():
+def get_driver(url):
     optionsForSearch = webdriver.ChromeOptions()
 
     #Set options to make browsing easier
@@ -27,18 +26,18 @@ def get_driver():
     
     optionsForSearch.add_experimental_option("excludeSwitches", ["enable-automation"])
     optionsForSearch.add_argument("disable-blank-features=AutomationControlled")
-    
+
     #fixes the problem of the tab immediately closing after the code execution
     optionsForSearch.add_experimental_option("detach", True)
+    
 
     
     driver = webdriver.Chrome(service=service, options=optionsForSearch)       #add the service attribute to fix the IDE problem
 
-    URL = "https://automated.pythonanywhere.com/"   #Connects driver to webpage, just enter the URL
-    driver.get(URL)
+   #Connects driver to webpage, just enter the URL
+    driver.get(url)
 
     return driver
-
 
 def clean_text(text):
     '''Filters and extracts only the temperature (the dynamic data) from the webpage'''
@@ -46,14 +45,23 @@ def clean_text(text):
     return output
 
 
-
-#For the xpath, ommit the /text() at the end
 def main():
-    driver = get_driver()
-    #This delay allows the program to retrieve the dynamic data, and not only the static one
-    time.sleep(2)
-    element = driver.find_element(by="xpath", 
-                                  value="/html/body/div[1]/div/h1[2]")
-    return clean_text(element.text)
+        driver = get_driver("https://automated.pythonanywhere.com/login/")
+        driver.find_element(by="id",
+                            value="id_username").send_keys("automated")
+        time.sleep(2)
+        driver.find_element(by="id",
+                            value="id_password").send_keys("automatedautomated" + Keys.RETURN)
+        time.sleep(2)
+        driver.find_element(by="xpath",
+                            value="/html/body/nav/div/a").click()
+        time.sleep(2)
+        element = driver.find_element(by="xpath",
+                                      value="/html/body/div[1]/div/h1[2]").text
+        
+        return clean_text(element)
+        
+print(main())
 
-main()
+
+
